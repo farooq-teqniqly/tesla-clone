bd6e451278c979e2f16eec1414d91ce86a29d0fc
farooq-teqniqly <farooq@teqniqly.com>
2024-01-10 21:00:08 -0800

    Added bouncy chevron

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -77,6 +77,18 @@ function HomePage() {
             </button>
           ))}
         </div>
+        <div className="absolute left-[50%] translate-x-[-50%] bottom-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-8 h-8 animate-bounce"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+          </svg>
+        </div>
       </div>
     </div>
   );
